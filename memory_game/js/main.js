be49f2af2df01c9305d4b9a04e9b1a8242e9b377
Bruno DaSilva <bananaatomica@Brunos-MacBook-Air.local>
2019-08-18 22:33:46 -0500

    Creating game logic, added JS

---
+++ memory_game/js/main.js
@@ -0,0 +1,23 @@
+/*
+
+Title:     Memory Game Main Style Sheet
+Author:    Bruno DaSilva
+File Type: JavaScrip  
+
+*/
+
+/*
+Step 1:
+Create two variables with a value of Queen 
+and two with the value of King
+*/
+
+var cardOne = 'queen'; 
+var cardTwo = 'queen'; 
+var cardThree = 'king'; 
+var cardFour = 'king'
+
+//console log two of the four cards to simulate a user interaction.
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
